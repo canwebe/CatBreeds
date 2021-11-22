c11b3988541c261373b21e1f5d8b32178754b446
StrikerOne65 <golamrabbani20014@gmail.com>
2021-11-22 18:49:53 +0530

    Adding 2 grid layout in about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -18,82 +18,90 @@ const About = ({
             <img src={state.url} alt='fullimg' />
 
             {/* <h2>{state.name}</h2> */}
-            <div className='content para'>
+            <div className='middleInfo'>
               <div className='catNameH2'>
                 <h2>{state.name}</h2>
               </div>
-              <div>
-                <p>
-                  <b>Origin:</b> {state.org}
-                </p>
-              </div>
-              <div className='gap20'>
-                <p>
-                  <b>Life_span:</b> {state.life}
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>Dog_friendly:</b>
-                  {/* {state.friendly} */}
-                </p>
-                <div className='progress'>
-                  <div
-                    style={{
-                      width: Math.round((state.friendly / 5) * 100) + '%',
-                    }}
-                    className='progress-done'
-                  ></div>
+              <div className='content para'>
+                <div className='topDesc'>
+                  <div>
+                    <p>
+                      <b>Origin:</b> {state.org}
+                    </p>
+                  </div>
+                  <div className='gap20'>
+                    <p>
+                      <b>Life_span:</b> {state.life}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p>
-                  <b>Child_friendly:</b>
-                  {/* {state.child_frd} */}
-                </p>
-                <div className='progress'>
-                  <div
-                    style={{
-                      width: Math.round((state.child_frd / 5) * 100) + '%',
-                    }}
-                    className='progress-done'
-                  ></div>
+                <div className='middleDesc'>
+                  <div className='meterBox'>
+                    <p>
+                      <b>Dog_friendly:</b>
+                      {/* {state.friendly} */}
+                    </p>
+                    <div className='progress'>
+                      <div
+                        style={{
+                          width: Math.round((state.friendly / 5) * 100) + '%',
+                        }}
+                        className='progress-done'
+                      ></div>
+                    </div>
+                  </div>
+                  <div className='meterBox'>
+                    <p>
+                      <b>Child_friendly:</b>
+                      {/* {state.child_frd} */}
+                    </p>
+                    <div className='progress'>
+                      <div
+                        style={{
+                          width: Math.round((state.child_frd / 5) * 100) + '%',
+                        }}
+                        className='progress-done'
+                      ></div>
+                    </div>
+                  </div>
+                  <div className='meterBox'>
+                    <p>
+                      <b>Energy_Level:</b>
+                      {/* {state.Energy} */}
+                    </p>
+                    <div className='progress'>
+                      <div
+                        style={{
+                          width: Math.round((state.Energy / 5) * 100) + '%',
+                        }}
+                        className='progress-done'
+                      ></div>
+                    </div>
+                  </div>
+                  <div className='meterBox'>
+                    <p>
+                      <b>Intelligence:</b>
+                      {/* {state.intell} */}
+                    </p>
+                    <div className='progress'>
+                      <div
+                        style={{
+                          width: Math.round((state.intell / 5) * 100) + '%',
+                        }}
+                        className='progress-done'
+                      ></div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p>
-                  <b>Energy_Level:</b>
-                  {/* {state.Energy} */}
-                </p>
-                <div className='progress'>
-                  <div
-                    style={{
-                      width: Math.round((state.Energy / 5) * 100) + '%',
-                    }}
-                    className='progress-done'
-                  ></div>
+                <div className='bottomDesc'>
+                  <div>
+                    <p>
+                      <b>Temperament:</b>
+                      <br />
+                      {state.temper}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className='gap20'>
-                <p>
-                  <b>Intelligence:</b>
-                  {/* {state.intell} */}
-                </p>
-                <div className='progress'>
-                  <div
-                    style={{
-                      width: Math.round((state.intell / 5) * 100) + '%',
-                    }}
-                    className='progress-done'
-                  ></div>
-                </div>
-              </div>
-              <div>
-                <p>
-                  <b>Temperament:</b>
-                  <br />
-                  {state.temper}
-                </p>
               </div>
             </div>
 
