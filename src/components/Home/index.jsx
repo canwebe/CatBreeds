8f8dd23f95a401cfe1b6677a8bd84b12c314d2b7
rohittiwari833 <rohittiwari7262@gmail.com>
2022-05-02 23:20:44 +0530

    added dog api

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,64 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "./Home.css";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
 
 const Home = ({ pageNo, setPageNo }) => {
-  const [image, setImage] = useState([]);
-  const [dataFull, setDataFull] = useState([]);
-  const [searchString, setSearchString] = useState("");
+  const [image, setImage] = useState([])
+  const [dataFull, setDataFull] = useState([])
+  const [searchString, setSearchString] = useState('')
   // const [pageNo, setPageNo] = useState(0)
 
   const handleNext = () => {
-    setPageNo((prev) => prev + 1);
-    window.scrollTo(0, 0);
-  };
+    setPageNo((prev) => prev + 1)
+    window.scrollTo(0, 0)
+  }
   const handlePrev = () => {
-    setPageNo((prev) => prev - 1);
-    window.scrollTo(0, 0);
-  };
+    setPageNo((prev) => prev - 1)
+    window.scrollTo(0, 0)
+  }
 
   const handleSearch = () => {
     fetch(
       `https://api.thecatapi.com/v1/breeds?api_key=${process.env.REACT_APP_API_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => setDataFull(data));
-  };
+      .then((data) => setDataFull(data))
+  }
 
   useEffect(() => {
     fetch(
       `https://api.thecatapi.com/v1/breeds?limit=12&page=${pageNo}&order=Asc&api_key=${process.env.REACT_APP_API_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => setImage(data));
-  }, [pageNo]);
+      .then((data) => setImage(data))
+  }, [pageNo])
 
   return (
-    <div className="home">
-      <div className="searchWrapper">
-        <div className="search wrapper">
-          <div className="input-link">
-            <h2>Search the name of Breed</h2>
-            <div className="searchBar">
-              <input
-                name="cName"
-                type="text"
-                value={searchString}
-                onChange={(e) => setSearchString(e.target.value.toLowerCase())}
-                onFocus={handleSearch}
-                placeholder="Eg: Abyssinian"
-              />
+    <div className='home'>
+      <div className='searchWrapper'>
+        <div className='search wrapper'>
+          <div className='searchBar'>
+            <input
+              name='cName'
+              type='text'
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value.toLowerCase())}
+              onFocus={handleSearch}
+              placeholder='Enter the Breed(eg: Abyssinian)'
+            />
 
-              <i className="fa fa-search"></i>
-            </div>
+            <i className='fa fa-search'></i>
           </div>
         </div>
       </div>
 
-      <div className="image">
-        <h2 className="cats">Cat's Lists</h2>
-        <div className="wrapper">
-          <div className="imgWrapper">
+      <div className='image'>
+        <h2 className='cats'>Cat's Lists</h2>
+        <div className='wrapper'>
+          <div className='imgWrapper'>
             {image.length ? (
               (searchString.length ? dataFull : image)
                 .filter((item) =>
@@ -69,7 +66,7 @@ const Home = ({ pageNo, setPageNo }) => {
                     item.image?.url && (
                       <Link
                         to={{
-                          pathname: "/info",
+                          pathname: '/info',
                           state: {
                             name: item.name,
                             desc: item.description,
@@ -86,20 +83,20 @@ const Home = ({ pageNo, setPageNo }) => {
                         }}
                         key={i}
                       >
-                        <div className="catCard">
-                          <img src={item.image.url} alt="catimage" />
-                          <div className="catName">{item.name}</div>
+                        <div className='catCard'>
+                          <img src={item.image.url} alt='catimage' />
+                          <div className='catName'>{item.name}</div>
                         </div>
                       </Link>
                     )
                 )
             ) : (
-              <p className="loadingImg"> Loading...</p>
+              <p className='loadingImg'> Loading...</p>
             )}
           </div>
           {image.length && (
             <div
-              className={`pagination ${searchString.length && "pageHidden"}`}
+              className={`pagination ${searchString.length && 'pageHidden'}`}
             >
               <button disabled={pageNo === 0} onClick={handlePrev}>
                 Prev
@@ -115,6 +112,6 @@ const Home = ({ pageNo, setPageNo }) => {
         </div>
       </div>
     </div>
-  );
-};
-export default Home;
+  )
+}
+export default Home
